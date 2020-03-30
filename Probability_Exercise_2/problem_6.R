8b591c0cde703cbbad5518acb8f04d94c3e3f838
Mingtao Huang <huangmt15@mails.tsinghua.edu.cn>
2020-03-30 13:11:28 +0800

    Fix a small error

--- Probability_Exercise_2/problem_6.R
+++ Probability_Exercise_2/problem_6.R
@@ -5,10 +5,18 @@ avg <- mean((1:N)^2)
 total <- sum((1:N)^2)
 
 E <- matrix(avg,N,N) # a 10-by-10 matrix and each element was assigned avg
+valid <- matrix(F,N-1,N-1)
 
-persp(1:10, 1:10, E, theta = 0, phi = 20,
-      expand = 0.5, col = 200,xlab = "S", ylab = "T", zlab = "Expectation", xlim = c(1,10), ylim = c(1,10),
-      zlim = c(35,55))
+for (s in 1:(N-1)) {
+  for (t in 1:(N-s) ) {
+    E[s,t] <- s*avg
+    valid[s,t-1] <- T
+  }
+}
+
+persp(1:10, 1:10, E, theta = -60, phi = 40,
+      expand = 0.5, col = 200-170*t(valid), xlab = "S", ylab = "T", zlab = "Expectation", xlim = c(1,10), ylim = c(1,10),
+      zlim = c(0,500))
 
 
 
@@ -25,12 +33,12 @@ for (n in 1:N_sim) {
   tmp <- tmp[-chosen] # delete the chosen doors
   open <- sample(tmp, t)
   if (mean(open^2) > avg) {
-    obtain[n] <- mean(chosen^2)
+    obtain[n] <- sum(chosen^2)
   } else {
     tmp <- 1:N
     tmp <- tmp[-c(chosen,open)] # delete the chosen doors
     chosen <- sample(tmp, s)
-    obtain[n] <- mean(chosen^2)
+    obtain[n] <- sum(chosen^2)
   }
 }
 
